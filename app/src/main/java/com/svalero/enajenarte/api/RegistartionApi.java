d9493ea0794812ecc1c2505ce0881b85e37b465e
PCaam-git <pcaamano@outlook.es>
2026-02-18 10:55:09 +0100

    Añadido Retrofit api interfaces

---
+++ app/src/main/java/com/svalero/enajenarte/api/RegistartionApi.java
@@ -0,0 +1,25 @@
+package com.svalero.enajenarte.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.time.LocalDate;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RegistrationApi {
+
+    public static RegistrationApiInterface buildInstance() {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        return retrofit.create(RegistrationApiInterface.class);
+    }
+}
