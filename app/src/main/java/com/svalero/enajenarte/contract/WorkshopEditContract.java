5c891ec4fb9ebc36eb739d22c9458cd65af6c323
PCaam-git <pcaamano@outlook.es>
2026-02-20 12:12:10 +0100

    Añadida operación put en Workshop. Modificaciones en la página de detalle para recargar los nuevos datos modificados

---
+++ app/src/main/java/com/svalero/enajenarte/contract/WorkshopEditContract.java
@@ -0,0 +1,22 @@
+package com.svalero.enajenarte.contract;
+
+import com.svalero.enajenarte.domain.Workshop;
+import com.svalero.enajenarte.domain.request.WorkshopRequest;
+
+public interface WorkshopEditContract {
+    public interface Model {
+        interface OnUpdateListener {
+            void onUpdateSuccess(Workshop workshop);
+            void onUpdateError(String message);
+        }
+        void updateWorkshop(long id, WorkshopRequest workshopRequest, OnUpdateListener listener);
+    }
+    public interface View {
+        void showMessage(String message);
+        void showError(String message);
+        void closeAfterUpdate();
+}
+    public interface Presenter {
+        void updateWorkshop(long id, WorkshopRequest workshopRequest);
+    }
+}
